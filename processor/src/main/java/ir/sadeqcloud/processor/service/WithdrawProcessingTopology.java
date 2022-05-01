16b0c56786cb24b60450a0a304e13dd0190c1e72
sadeq <tsts220g@gmail.com>
2022-05-01 18:17:32 +0430

    branch topology into reverse or withdraw processing

--- processor/src/main/java/ir/sadeqcloud/processor/service/WithdrawProcessingTopology.java
+++ processor/src/main/java/ir/sadeqcloud/processor/service/WithdrawProcessingTopology.java
@@ -1,6 +1,7 @@
 package ir.sadeqcloud.processor.service;
 
 import ir.sadeqcloud.processor.constants.PropertyConstants;
+import ir.sadeqcloud.processor.model.RequestType;
 import ir.sadeqcloud.processor.model.TransferRequest;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
@@ -10,6 +11,7 @@ import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.support.KafkaStreamBrancher;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,10 +24,23 @@ public class WithdrawProcessingTopology {
         this.transferRequestSerde=transferRequestSerde;
         this.accountOperations=accountOperations;
     }
-    @Bean
+    @Bean("topologySourceNode")
     public KStream<String,TransferRequest> sourceProcessing(StreamsBuilder streamsBuilder){
     return streamsBuilder.stream(PropertyConstants.getInputTopic(),
-            Consumed.with(Serdes.String(),transferRequestSerde).withName("source-node")).
-            filter((k,v)->accountOperations.checkWithdrawLimitationThresholdNotPassed(v));
+            Consumed.with(Serdes.String(),transferRequestSerde).withName("source-node"));
+    }
+    @Bean
+    private void branchStream(@Qualifier("topologySourceNode") KStream<String,TransferRequest> sourceNode){
+         new KafkaStreamBrancher<String, TransferRequest>().
+                 branch((string, transferRequest) -> transferRequest.getRequestType() == RequestType.PROCEED_WITHDRAW, ks -> withdrawProcessing(ks))
+                 .branch((string,transferRequest)->transferRequest.getRequestType()==RequestType.REVERSE,ks->reverseProcessing(ks)).
+                 onTopOf(sourceNode);
+    }
+
+    public KStream withdrawProcessing(KStream<String,TransferRequest> withdrawBranch){
+        return withdrawBranch.filter((k,v)->accountOperations.checkWithdrawLimitationThresholdNotPassed(v));
+    }
+    public void reverseProcessing(KStream<String,TransferRequest> reverseBranch){
+
     }
 }
